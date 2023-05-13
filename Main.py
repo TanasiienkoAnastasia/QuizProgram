3e43ba108379c0238db7aad2b5ee8b5191235ae3
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-05-13 21:02:18 +0300

    create the dictionary that stores questions and answers

---
+++ Main.py
@@ -0,0 +1,30 @@
+quiz = {
+    "question1": {
+        "question1": "What is the capital of the France?",
+        "answer": "Paris"
+    },
+    "question2": {
+        "question2": "What is the capital of the Germany?",
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question3": "What is the capital of the Italy?",
+        "answer": "Rome"
+    },
+    "question4": {
+        "question4": "What is the capital of the Spain?",
+        "answer": "Madrid"
+    },
+    "question5": {
+        "question5": "What is the capital of the Portugal?",
+        "answer": "Lisbon"
+    },
+    "question6": {
+        "question6": "What is the capital of the Switzerland?",
+        "answer": "Bern"
+    },
+    "question7": {
+        "question7": "What is the capital of the Austria?",
+        "answer": "Vienna"
+    },
+}
