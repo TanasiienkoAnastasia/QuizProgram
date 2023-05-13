9fce446ca2bc03e87e21a2d7b1ab688c0c5222c0
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-05-13 23:03:10 +0300

    display questions, allow to answer, tell if they are wrong  or right

--- Main.py
+++ Main.py
@@ -24,3 +24,17 @@ quiz = {
 }
 
 score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer? ")
+
+    if answer.lower() == value['answer'].lower():
+        print("Correct")
+        score = score + 1
+        print("Your score is: " + str(score))
+
+    else:
+        print("Wrong!")
+        print("The answer is : " + value['answer'])
+        print("Your score is: " + str(score))
